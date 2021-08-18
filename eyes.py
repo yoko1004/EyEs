dd28c24963d82ff680919224bc46786ec7f6420b
yoko1004 <88901888+yoko1004@users.noreply.github.com>
2021-08-18 14:01:54 +0900

    Update eyes.py

--- eyes.py
+++ eyes.py
@@ -1,13 +1,14 @@
-import uiautomation as ui
-import  mouse
 from time import sleep
-from uisoup import uisoup
-import winsound
-import pyttsx3
-import wx
 import threading
 import unicodedata
-import keyboard
+import winsound
+
+import uiautomation as ui #サードパーティ uiautomation
+from uisoup import uisoup #サードパーティ MSAA
+import pyttsx3 #サードパーティ 読み上げ
+import wx #サードパーティ GUI
+import  mouse #サードパーティ マウス関連
+import keyboard #サードパーティ キーボード関連
 
 
 class MainFrame(wx.Frame): #wxPythonの設定
@@ -22,40 +23,40 @@ class MainFrame(wx.Frame): #wxPythonの設定
         layout.Add(self.text,wx.CENTER)
         self.panel.SetSizer(layout)
 
-    def text_change(self,text):
+    def text_change(self,text): #テキストを変える
         self.text.SetLabel(text)
 
-def get_mouse():
+def get_mouse(): #マウスの位置を取得
     global old_pos
     new_pos = mouse.get_position()
-    if new_pos != old_pos:
+    if new_pos != old_pos: #前回拾った座標と違ったらget_by_uiを実行する
         old_pos = new_pos
         get_by_ui(old_pos[0],old_pos[1])
 
-def get_by_ui(x,y):
+def get_by_ui(x,y): #uiautomationを利用して情報を取得する
     global old_elems_ui
     global old_elems_msaa
     try:
-        new_elems_ui = ui.ControlFromPoint(x,y)
-        if new_elems_ui.Name != old_elems_ui.Name and new_elems_ui.Name != '':
+        new_elems_ui = ui.ControlFromPoint(x,y) #ここで取得
+        if new_elems_ui.Name != old_elems_ui.Name and new_elems_ui.Name != '': #前回の情報と異なり、値が入ってたら...
             old_elems_ui = new_elems_ui
-            if old_elems_ui.Name != old_elems_msaa:
+            if old_elems_ui.Name != old_elems_msaa: #前回のMSAAと比べる
                 print("ui-----" + str(old_elems_ui.Name) + "/")
-                speak(str(old_elems_ui.Name))
+                speak(str(old_elems_ui.Name)) #話す
         else:
-            get_by_msaa(x,y)
+            get_by_msaa(x,y) #Falseだったらget_by_msaaを実行する
     except:
         get_by_msaa(x,y)
 
-def get_by_msaa(x,y):
+def get_by_msaa(x,y): #MSAAを利用して情報を取得する
     global old_elems_ui
     global old_elems_msaa_list
     global old_elems_msaa
     try:
-        new_elems_msaa = uisoup.get_object_by_coordinates(x,y)
+        new_elems_msaa = uisoup.get_object_by_coordinates(x,y) #ここで取得
         new_elems_msaa_list = []
 
-        get_value_key_list =['acc_role_name','acc_name','acc_value']
+        get_value_key_list =['acc_role_name','acc_name','acc_value'] #色々付いてくるのでこの3つだけを取得する
         ''''''
         for key in get_value_key_list:
             try:
@@ -63,44 +64,32 @@ def get_by_msaa(x,y):
                 new_elems_msaa_list.append(elem)
             except:
                 new_elems_msaa_list.append('')
-        if new_elems_msaa_list != old_elems_msaa_list and new_elems_msaa_list[1] != '':
+        if new_elems_msaa_list != old_elems_msaa_list and new_elems_msaa_list[1] != '': #前回の情報と異なり、値が入ってたら...
             old_elems_msaa_list = new_elems_msaa_list
-            '''
-            if old_elems_msaa_list[0] == "txt":
-                print("msaa---" + str(old_elems_msaa_list[2]))
-            else:
-                print("msaa---" + str(old_elems_msaa_list[1]))
-            if old_elems_msaa_list[0] == "txt":
-                pass
-            '''
-            if old_elems_msaa_list[0] == "txt":
+            
+            if old_elems_msaa_list[0] == "txt": #テキストオブジェクトだったら...
                 old_elems_msaa = old_elems_msaa_list[2]
             else:
                 old_elems_msaa = old_elems_msaa_list[1]
-            if old_elems_msaa != old_elems_ui.Name:
+            if old_elems_msaa != old_elems_ui.Name: #前回のuiautomationと比べる
                 print("msaa---" + str(old_elems_msaa) + "/")
                 speak(str(old_elems_msaa))
-            #print(str(new_elems_msaa_list))
     except:
         pass
         
-
-
-def speak(text):
+def speak(text): #話す
     pass
 
     global engine
     global layout
-    engine.save_to_file(text, "sound/output.mp3")
+    engine.save_to_file(text, "sound/output.mp3") #ここで音声ファイルを書き込む
     engine.runAndWait()
-    winsound.PlaySound("sound/output.mp3",winsound.SND_ASYNC)
+    winsound.PlaySound("sound/output.mp3",winsound.SND_ASYNC) #再生する
     #frame.text.SetLabel(text_count(text))
 
-    frame.text_change(text_count(text))
-    
+    frame.text_change(text_count(text)) #GUIに書き込む（その前に自動改行を実行する）
 
-
-def text_count(text):
+def text_count(text): #自動改行
     c = 0
     text_list = list()
     for w in text:
@@ -116,13 +105,13 @@ def text_count(text):
 
     return text
 
-def key_speak():
+def key_speak(): #押したキーを通知する
     global old_key
     global engine
     count = 0
-    engine2 = pyttsx3.init()
+    engine2 = pyttsx3.init() #初期化
     while True:
-        new_key = keyboard.read_key()
+        new_key = keyboard.read_key() #押されたキーを取得
         if new_key != old_key:
             old_key = new_key
             engine2.save_to_file(old_key, "sound/output_key.mp3")
@@ -138,22 +127,21 @@ def main():
     while True:
         get_mouse()
         sleep(0.1)
-
-
+        
 if __name__ == "__main__":
-    old_pos = mouse.get_position()
+    old_pos = mouse.get_position() 
     old_elems_ui = ui.ControlFromPoint(old_pos[0],old_pos[1])
     old_elems_msaa_list = []
     old_elems_msaa = ""
     old_key = ""
-    engine = pyttsx3.init()
-    app = wx.App(False)
-    frame = MainFrame()
+    engine = pyttsx3.init() #初期化
+    app = wx.App(False) #GUI作成
+    frame = MainFrame() #GUIの中身を作る
     frame.Show()
-    thread1 = threading.Thread(target=main)
+    thread1 = threading.Thread(target=main) #別スレッドで実行
     thread1.setDaemon(True)
     thread1.start()
-    thread2 = threading.Thread(target=key_speak)
+    thread2 = threading.Thread(target=key_speak) #別スレッドで実行
     thread2.setDaemon(True)
     thread2.start()
-    app.MainLoop()
+    app.MainLoop() #GUIループ
